0c5a324b22967e2d15a86f9490ffc19829d38c34
Glitch (hello-webpage) <none>
2020-12-23 14:47:39 +0000

    🍣🍿 Checkpoint ./index.html:24336113/713 ./app.js:24336113/2933 ./background.js:24336113/1830 ./main.js:24336113/1605

---
+++ background.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	let canvas = document.getElementById("cvs");
+  	let context = canvas.getContext("2d");
+  	
+
+  	context.fillStyle = "#112";
+  	context.fillRect(0 , canvas.height, 10, -35);
+  	context.fillRect(10, canvas.height, 10, -15);
+  	context.fillRect(20, canvas.height, 10, -20);
+  	context.fillRect(30, canvas.height, 10, -30);
+  	context.fillRect(40, canvas.height, 10, -25);
+  	context.fillRect(50, canvas.height, 10, -10);
+  	context.fillRect(60, canvas.height, 10, -20);
+  	context.fillRect(70, canvas.height, 10, -35);
+  	context.fillRect(80, canvas.height, 10, -35);
+  	context.fillRect(90, canvas.height, 10, -15);
+  	context.fillRect(100, canvas.height, 10, -20);
+  	context.fillRect(110, canvas.height, 10, -35);
+  	context.fillRect(120, canvas.height, 10, -10);
+  	context.fillRect(130, canvas.height, 10, -15);
+  	context.fillRect(140, canvas.height, 10, -10);
+  	context.fillRect(150, canvas.height, 10, -35);
+  	context.fillRect(160, canvas.height, 10, -20);
+  	context.fillRect(170, canvas.height, 10, -15);
+  	context.fillRect(180, canvas.height, 10, -25);
+  	context.fillRect(190, canvas.height, 10, -20);
+  	context.fillRect(200, canvas.height, 10, -25);
+  	context.fillRect(210, canvas.height, 10, -10);
+  	context.fillRect(220, canvas.height, 10, -20);
+  	context.fillRect(230, canvas.height, 10, -35);
+  	context.fillRect(240, canvas.height, 10, -25);
+  	context.fillRect(250, canvas.height, 10, -20);
+  	context.fillRect(260, canvas.height, 10, -25);
+  	context.fillRect(270, canvas.height, 10, -10);
+  	context.fillRect(280, canvas.height, 10, -20);
+  	context.fillRect(290, canvas.height, 10, -35);
+  var imagePaper = new Image();
+
+
+        imagePaper.onload = function(){
+            context.drawImage(imagePaper,250,20,20,20);
+        };
+
+      imagePaper.src = "moon.png";
+  
+
+
+});
---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  var countDownDate = new Date("2021 Jan 01 00:00:00").getTime();
+  var sayTime = new Date("2020 Dec 31 23:59:50").getTime();
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+     // Display the result in the element with id="demo"
+     document.getElementById("day").innerHTML = "<span>" + days + "</span>DAYS";
+     document.getElementById("hour").innerHTML = "<span>" + hours + "</span>HOURS";
+     document.getElementById("minute").innerHTML = "<span>" + minutes + "</span>MINUTES";
+     document.getElementById("second").innerHTML = "<span>" + seconds + "</span>SECOND";
+     // If the count down is finished, write some text
+      if(now-(now%1000)==sayTime){
+        var audio = new Audio("countdownsound.mp3");
+        audio.play();
+      }
+      if (distance <= 0) {
+        clearInterval(x);
+        document.getElementById("container").innerHTML = "<h1>HAPPY NEW YEAR <br> <span>2021</span> </h1>";
+        var audio = new Audio("Song.mp3");
+        audio.play();
+        Run();
+      
+      }
+
+  }, 1000);
+});
+
+
